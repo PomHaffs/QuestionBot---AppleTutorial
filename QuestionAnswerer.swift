9b8fb01581e823a724f86c1f09c7a633522b77ea
PomHaffs <tomashaffenden@icloud.com>
2016-11-11 13:47:07 +1100

    Completed with basic logic

--- QuestionAnswerer.swift
+++ QuestionAnswerer.swift
@@ -1,6 +1,32 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response 
-        return "?"
+        
+        let question = question.lowercased()
+        
+        if question == "who is Ruth?" {
+            return "She is the most beautiful and perfect girl in all the world!"
+        } else if question.hasPrefix("hello") {
+            return "Hello beautiful"
+        } else if question.hasPrefix("where") {
+            return "Just down the road and take a left"
+        } else if question.hasPrefix("what") {
+            return "The question should not be what, it should be when!"
+        } else if question.hasPrefix("when") {
+            return "Probably tomorrow"
+        } else if question.hasPrefix("how") {
+            return "With a lot of hard work I would imagine"
+        }
+        else {
+            let defaultNumber = question.characters.count
+            
+            if defaultNumber == 10 {
+                return "That really depends"
+            } else if defaultNumber < 10 {
+                return "Is that all you have to say?"
+            } else {
+                return "shit biscuits!, you are wasting my time!"
+            }
+        }
+        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # QuestionBot---AppleTutorial
+
+Part 2 of Apple Tutorial
+
+Description: single page app with question answerinf feature. Some basic logic writen to convert String to lowercase before using hasPrefix. Answers given based on 'when', 'where' etc...
